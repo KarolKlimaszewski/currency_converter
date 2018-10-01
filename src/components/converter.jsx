1ebb858e8b4ba44150229341884e69e9b1067071
Karol Klimaszewski <karol.klimaszewski1997@gmail.com>
2018-10-01 18:34:13 +0200

    Added conversion from user rate to target rate

--- src/components/converter.jsx
+++ src/components/converter.jsx
@@ -6,7 +6,10 @@ export class Converter extends React.Component {
         super(props);
         this.state = {
             data: "",
-            userCurrency: []
+            userCurrency: "choose",
+            targetCurrency: "choose",
+            amount: 0,
+            displayResult: "none"
         }
     }
 
@@ -14,6 +17,42 @@ export class Converter extends React.Component {
         fetch('https://api.exchangeratesapi.io/latest?base=PLN')
             .then(response => response.json())
             .then(data => this.setState({data}));
+    }
+
+    handleUserCurrencyChange = (event) => {
+        this.setState({
+            userCurrency: event.target.value
+        })
+    }
+
+    handleTargetCurrencyChange = (event) => {
+        this.setState({
+            targetCurrency: event.target.value
+        })
+    }
+
+    handleAmountChange = (event) => {
+        this.setState({
+            amount: event.target.value
+        })
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        if(this.state.userCurrency !== "choose" && this.state.targetCurrency !== "choose" && this.state.amount !== 0) {
+        this.setState({
+            communicate: "",
+            displayResult: "block"
+        })
+            fetch('https://api.exchangeratesapi.io/latest?base='+this.state.userCurrency)
+                .then(response => response.json())
+                .then(data => this.setState({data}));
+        }
+        else{
+            this.setState({
+                communicate: "please fill all fields."
+            })
+        }
     }
 
     render() {
@@ -32,19 +71,26 @@ export class Converter extends React.Component {
                     <p>
                         I want to convert
                     </p>
-                    <input type="text" placeholder={"Write amount here..."}/>
-                    <select className={"addOperation__select"} name="currency">
+                    <input type="text" placeholder={"Write amount here..."} value={this.state.amount} onChange={this.handleAmountChange}/>
+                    <select className={"addOperation__select"} name="currency" onChange={this.handleUserCurrencyChange}>
                         <option className={"currency__choose"} value="choose">Choose</option>
                         {currencies}
                     </select>
                     <p>
                         to:
                     </p>
-                    <select className={"addOperation__select"} name="currency">
+                    <select className={"addOperation__select"} name="currency" onChange={this.handleTargetCurrencyChange}>
                         <option className={"currency__choose"} value="choose">Choose</option>
                         <option className={"currency__choose"} value="all">All</option>
                         {currencies}
                     </select>
+                    <button className="submit" onClick={this.handleSubmit}>{">"}</button>
+
+                    <div className="results" style={{display: this.state.displayResult}}>
+                        Conversion rate: 1 {this.state.userCurrency} =
+                        {this.state.data.rates[this.state.targetCurrency]} {this.state.targetCurrency} <br/>
+                        Amount: {this.state.data.rates[this.state.targetCurrency] * this.state.amount}
+                    </div>
                 </div>
             )
         } else {
