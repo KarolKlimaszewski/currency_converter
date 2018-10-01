6d6e3a73a1a3f67eac25f4a738c25eeb4ed4ad23
Karol Klimaszewski <karol.klimaszewski1997@gmail.com>
2018-10-01 17:30:47 +0200

    Added fetch and state update after getting data

--- src/components/converter.jsx
+++ src/components/converter.jsx
@@ -1,13 +1,58 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-export class Converter extends React.Component {
-   constructor(props){
-     super(props);
 
-   }
-   render() {
-     return (
-       <h1>Hello</h1>
-     )
-   }
- }
+export class Converter extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: "",
+            userCurrency: []
+        }
+    }
+
+    componentDidMount() {
+        fetch('https://api.exchangeratesapi.io/latest?base=PLN')
+            .then(response => response.json())
+            .then(data => this.setState({data}));
+    }
+
+    render() {
+        if (this.state.data) {
+            const currenciesArr = [];
+            for(let name in this.state.data.rates){
+                currenciesArr.push(name);
+            }
+            let currencies = currenciesArr.map((el, i) => {
+                return <option key={i} className={"currency__choose"} value={el}>{el}</option>
+            })
+
+
+            return (
+                <div>
+                    <p>
+                        I want to convert
+                    </p>
+                    <input type="text" placeholder={"Write amount here..."}/>
+                    <select className={"addOperation__select"} name="currency">
+                        <option className={"currency__choose"} value="choose">Choose</option>
+                        {currencies}
+                    </select>
+                    <p>
+                        to:
+                    </p>
+                    <select className={"addOperation__select"} name="currency">
+                        <option className={"currency__choose"} value="choose">Choose</option>
+                        <option className={"currency__choose"} value="all">All</option>
+                        {currencies}
+                    </select>
+                </div>
+            )
+        } else {
+            return (
+                <div>
+                    loading...
+                </div>
+            )
+        }
+    }
+}
