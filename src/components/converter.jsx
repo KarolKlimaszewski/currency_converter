303909ec486dc0857cb28661574fe24efca8b2c0
Karol Klimaszewski <karol.klimaszewski1997@gmail.com>
2018-10-03 21:41:39 +0200

    Added functionality to check historical conversion rate

--- src/components/converter.jsx
+++ src/components/converter.jsx
@@ -8,14 +8,26 @@ export class Converter extends React.Component {
         super(props);
         this.state = {
             data: "",
+            hData: "",
             selectedoption: null,
             userCurrency: "",
             targetCurrency: "",
             amount: "",
-            displayResult: "none",
-            displayAmount: "",
-            displayUserCurrency: "",
-            displayTargetCurrency: ""
+            display: {
+                result: "none",
+                amount: "",
+                userCurrency: "",
+                targetCurrency: ""
+            },
+            date: "",
+            hUserCurrency: "",
+            hTargetCurrency: "",
+            hDisplay: {
+                hResult: "none",
+                hDate: "",
+                hUserCurrency: "",
+                hTargetCurrency: "",
+            },
         }
     }
 
@@ -23,7 +35,12 @@ export class Converter extends React.Component {
         fetch('https://api.exchangeratesapi.io/latest?base=PLN')
             .then(response => response.json())
             .then(data => this.setState({data}));
+        fetch('https://api.exchangeratesapi.io/2018-01-01')
+            .then(response => response.json())
+            .then(hData => this.setState({hData}));
     }
+
+    //Methods for converter
 
     handleUserCurrencyChange = (e) => {
         this.setState({
@@ -51,12 +68,13 @@ export class Converter extends React.Component {
         if (this.state.userCurrency && this.state.targetCurrency && this.state.amount) {
             this.setState({
                 communicate: "",
-                displayResult: "block",
                 amount: Math.round(this.state.amount, 3),
-                displayUserCurrency: this.state.userCurrency,
-                displayTargetCurrency: this.state.targetCurrency,
-                displayAmount: this.state.amount
-
+                display: {
+                    result: "block",
+                    amount: this.state.amount,
+                    userCurrency: this.state.userCurrency,
+                    targetCurrency: this.state.targetCurrency
+                },
             })
             fetch('https://api.exchangeratesapi.io/latest?base=' + this.state.userCurrency)
                 .then(response => response.json())
@@ -65,10 +83,58 @@ export class Converter extends React.Component {
         else {
             this.setState({
                 communicate: "please fill all fields.",
-                displayResult: "none"
+                display: {
+                    result: "none"
+                },
             })
         }
     }
+
+    //Methods for historical
+
+    handleDateChange = (event) => {
+            this.setState({
+                date: event.target.value
+            })
+    }
+
+    historicalUserCurrencyChange = (e) => {
+        this.setState({
+            hUserCurrency: e.value,
+        })
+    }
+
+    historicalTargetCurrencyChange = (e) => {
+        this.setState({
+            hTargetCurrency: e.value,
+        })
+    }
+
+    historicalSubmit = (e) => {
+        e.preventDefault();
+        if (this.state.date && this.state.hUserCurrency && this.state.hTargetCurrency) {
+            this.setState({
+                hDisplay: {
+                    hResult: "block",
+                    hDate: this.state.date,
+                    hUserCurrency: this.state.hUserCurrency,
+                    hTargetCurrency: this.state.hTargetCurrency
+                },
+            })
+            fetch('https://api.exchangeratesapi.io/' + this.state.date)
+                .then(response => response.json())
+                .then(hData => this.setState({hData}));
+        }
+        else {
+            this.setState({
+                hDisplay: {
+                    hResult: "none"
+                },
+            })
+        }
+    }
+
+
 
     render() {
         if (this.state.data) {
@@ -110,28 +176,75 @@ export class Converter extends React.Component {
                         </div>
                     </section>
 
-                    <div className="results" style={{display: this.state.displayResult}}>
+                    <section className="results" style={{display: this.state.display.result}}>
                         <p className="results__amountFrom results__amount">
-                            {this.state.displayAmount} {this.state.displayUserCurrency} =
+                            {this.state.display.amount} {this.state.display.userCurrency} =
                         </p>
                         <p className="results__amountTo results__amount">
-                            {Math.round(this.state.data.rates[this.state.displayTargetCurrency] * this.state.displayAmount * 1000) / 1000}&nbsp;
-                            {this.state.displayTargetCurrency}
+                            {Math.round(this.state.data.rates[this.state.display.targetCurrency] * this.state.display.amount * 1000) / 1000}&nbsp;
+                            {this.state.display.targetCurrency}
                         </p>
                         <p className="results__conversionFrom results__conversion">
-                            1 {this.state.displayUserCurrency} =&nbsp;
-                            {Math.round(this.state.data.rates[this.state.displayTargetCurrency] * 100000) / 100000} &nbsp;
-                            {this.state.displayTargetCurrency}
+                            1 {this.state.display.userCurrency} =&nbsp;
+                            {Math.round(this.state.data.rates[this.state.display.targetCurrency] * 100000) / 100000} &nbsp;
+                            {this.state.display.targetCurrency}
                         </p>
                         <p className="results__conversionTo results__conversion">
-                            1 {this.state.displayTargetCurrency} =&nbsp;
-                            {Math.round(1 / this.state.data.rates[this.state.displayTargetCurrency] * 100000) / 100000} &nbsp;
-                            {this.state.displayUserCurrency}
+                            1 {this.state.display.targetCurrency} =&nbsp;
+                            {Math.round(1 / this.state.data.rates[this.state.display.targetCurrency] * 100000) / 100000} &nbsp;
+                            {this.state.display.userCurrency}
                         </p>
                         <p className="results__date">
                             Last update: {this.state.data.date}
                         </p>
-                    </div>
+                    </section>
+                    <section className="historical">
+                        <div className="historical__container col-12 col-md-6 col-lg-3">
+                            <h2 className="historical__title">
+                                Historical conversion rates
+                            </h2>
+                            <p className={"historical__text"}>
+                                Date:
+                            </p>
+                            <input className={"converter__input"} type="date" placeholder={"Write date here..."}
+                                   value={this.state.date} onChange={this.handleDateChange}/>
+                        </div>
+                        <div className="converter__container col-12 col-md-6 col-lg-3">
+                            <p className={"converter__text"}>
+                                from:
+                            </p>
+                            <Select onChange={this.historicalUserCurrencyChange} placeholder={"Your currency..."}
+                                    className={"converter__select"} options={currencies}/>
+                        </div>
+                        <div className="converter__container col-12 col-md-6 col-lg-3">
+                            <p className={"converter__text"}>
+                                to:
+                            </p>
+                            <Select onChange={this.historicalTargetCurrencyChange} placeholder={"Target currency..."}
+                                    className={"converter__select"} options={currencies}/>
+                        </div>
+                        <div className="converter__container col-12 col-md-6 col-lg-3">
+                            <button className="converter__submit" onClick={this.historicalSubmit}></button>
+                        </div>
+                    </section>
+                    <section className="results" style={{display: this.state.hDisplay.hResult}}>
+                        <p className="results__amountFrom results__amount">
+                            {this.state.hDisplay.hDate}
+                        </p>
+                        <p className="results__conversionFrom results__conversion">
+                            1 {this.state.hDisplay.hUserCurrency} =&nbsp;
+                            {Math.round(this.state.hData.rates[this.state.hDisplay.hTargetCurrency] * 100000) / 100000} &nbsp;
+                            {this.state.hDisplay.hTargetCurrency}
+                        </p>
+                        <p className="results__conversionTo results__conversion">
+                            1 {this.state.hDisplay.hTargetCurrency} =&nbsp;
+                            {Math.round(1 / this.state.hData.rates[this.state.hDisplay.hTargetCurrency] * 100000) / 100000} &nbsp;
+                            {this.state.hDisplay.hUserCurrency}
+                        </p>
+                        <p className="results__date">
+                            Last update: {this.state.hData.date}
+                        </p>
+                    </section>
                 </div>
             )
         } else {
